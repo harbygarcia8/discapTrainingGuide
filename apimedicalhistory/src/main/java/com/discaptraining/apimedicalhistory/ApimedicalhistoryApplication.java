6fb9f6a5f569d36f3e90582effc6c64aab76879c
harby garcia <harby-96@hotmail.com>
2023-04-19 20:56:08 -0500

    [INITIAL] initial commit microservices clinicalhystory

---
+++ apimedicalhistory/src/main/java/com/discaptraining/apimedicalhistory/ApimedicalhistoryApplication.java
@@ -0,0 +1,13 @@
+package com.discaptraining.apimedicalhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimedicalhistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimedicalhistoryApplication.class, args);
+	}
+
+}
---
+++ apimedicalhistory/src/test/java/com/discaptraining/apimedicalhistory/ApimedicalhistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discaptraining.apimedicalhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimedicalhistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
